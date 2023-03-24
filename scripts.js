fdb2ae573fea0761420c37e1de20ec9ae32a05d6
Erik Z <molagg92@gmail.com>
2023-03-24 16:39:23 -0700

    will try to finalize my project, trying to writ teh if else statements

--- scripts.js
+++ scripts.js
@@ -1,9 +1,97 @@
-window.addEventListener("load", function() {
-  form = this.document.querySelector("form");
-  form.addEventListener("submit", )
-});
 
-function surveySubmition(e) {
-  e.preventDefault();
-  
-} 
+
+const range = document.querySelector("range");
+const defences = document.getElementById('defences');
+const role = document.getElementById('role');
+const training = document.getElementById('training');
+const pet = document.getElementById('pet');
+
+const pythonMan= document.getElementById('pythonMan');
+const rubyLas = document.getElementById('rubyLas');
+const swiftWoman = document.getElementById('rangerWoman');
+
+const rangeChoice = range.querySelector("input:checked").id;
+const defenceChoice = defences.querySelector("input:checked").id;
+const roleChoice = role.querySelector("input:checked").id;
+const trainingChoice = training.querySelector("input:checked").id;
+const petChoice = pet.querySelector("input:checked").id;
+
+let result;
+
+if (rangeChoice === 'close' && defenceChoice === 'tough' && roleChoice === 'leader') {
+  let result = pythonMan;
+} else if (rangeChoice === 'distance' && defenceChoice === 'smart' && roleChoice === 'support') {
+  let result = rubyLas;
+} else if (rangeChoice === '50/50' && defenceChoice === 'slick' && roleChoice === 'slick') {
+  let result = swiftWoman;
+}
+
+
+
+if (result === pythonMan) {
+  pythonMan.removeAttribute('class');
+} else if (result === rubyLas) {
+  rubyLas.removeAttribute('class');
+} else if (result === swiftWoman) {
+  swiftWoman.removeAttribute('class')
+}
+window.addEventListener("load", function() {
+  const  form = document.querySelector("form");
+  form.addEventListener("submit", surveySubmition)
+});
+//   // determine the result based on the user's choices
+//   let result;
+//   if (rangeChoice === 'close' && defencesChoice === 'slick' && roleChoice === 'leader' && trainingChoice === 'dojo' && petChoice === 'owl') {
+//     result = 'ruby';
+//   } else if (rangeChoice === 'distance' && defencesChoice === 'tough' && roleChoice === 'solo' && trainingChoice === 'aim' && petChoice === 'snake') {
+//     result = 'swift';
+//   } else {
+//     result = 'python';
+//   }
+
+//   // hide all results
+//   pythonResult.classList.add('hidden');
+//   rubyResult.classList.add('hidden');
+//   swiftResult.classList.add('hidden');
+
+//   // show the corresponding result
+//   if (result === 'python') {
+//     pythonResult.classList.remove('hidden');
+//   } else if (result === 'ruby') {
+//     rubyResult.classList.remove('hidden');
+//   } else {
+//     swiftResult.classList.remove('hidden');
+//   }
+// });
+
+
+
+
+// window.addEventListener("load", function() {
+//   const  form = this.document.querySelector("form");
+//   form.addEventListener("submit", surveySubmition)
+// });
+
+// function surveySubmition(e) {
+//   e.preventDefault();
+//   const close = document.getElementById("close").value;
+//   const distance = document.getElementById("distance").value;
+//   const dual = document.getElementById("50/50").value;
+
+//   const slick = document.getElementById("slick").value;
+//   const tough = document.getElementById("tough").value;
+//   const smart = document.getElementById("smart").value;
+
+//   const leader = document.getElementById("leader").value;
+//   const support = document.getElementById("support").value;
+//   const solo = document.getElementById("solo").value;
+
+//   const library = document.getElementById("library").value;
+//   const dojo = document.getElementById("dojo").value;
+//   const aim = document.getElementById("aim").value;
+
+//   const boar = document.getElementById("boar").value;
+//   const owl = document.getElementById("owl").value;
+//   const snake = document.getElementById("snake").value;
+
+// } 
