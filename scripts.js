5fd61111d2638f41f03fbc5dc873c61d36fe986b
Erik Z <molagg92@gmail.com>
2023-03-24 12:57:05 -0700

    fill out more of the html, defined the survey questiones more. Its's finally time to take a crack at this javascript

---
+++ scripts.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function() {
+  form = this.document.querySelector("form");
+  form.addEventListener("submit", )
+});
+
+function surveySubmition(e) {
+  e.preventDefault();
+  
+} 
