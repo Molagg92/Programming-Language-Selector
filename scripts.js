6213950a25424e4e5757e36e2d5bca6185b88eff
Erik Z <molagg92@gmail.com>
2023-03-26 08:15:19 -0700

    will add if else statments, turning this into a minimum viable product! I dont know whats going on whith my last else statment tho...

--- scripts.js
+++ scripts.js
@@ -6,12 +6,41 @@ function surveySubmission(e) {
   e.preventDefault();
 
   const rangeInput = document.querySelector('input[name="range"]:checked').value;
+  const defencesInput = document.querySelector('input[name="defences"]:checked').value;
+  const roleInput = document.querySelector('input[name="role"]:checked').value;
+  // const trainingInput = document.querySelector('input[name="training"]:checked').value;
+  // const petInput = document.querySelector('input[name="pet"]:checked').value;
+
+
   const pythonMan= document.getElementById('pythonMan');
+  const rubyLas = document.getElementById('rubyLas');
+  const swiftWoman = document.getElementById('rangerWoman');
+  const tryAgain = document.getElementById('tryAgain');
+
   let result = "undefined"
   
-  if (rangeInput === "close") {
+  if (rangeInput === "close" && defencesInput === "tough" && roleInput === "leader") {
     pythonMan.removeAttribute('class');
     result = pythonMan;
+    rubyLas.setAttribute("class", "hidden");
+    swiftWoman.setAttribute("class", "hidden");
+  } else if (rangeInput === "distance" && defencesInput === "smart" && roleInput === "support"){
+    rubyLas.removeAttribute('class');
+    result = rubyLas;
+    pythonMan.setAttribute("class", "hidden");
+    swiftWoman.setAttribute("class", "hidden");
+  } else if (rangeInput === "50/50" && defencesInput === "slick" && roleInput === "solo"){
+    swiftWoman.removeAttribute('class');
+    result = swiftWoman;
+    pythonMan.setAttribute("class", "hidden");
+    rubyLas.setAttribute("class", "hidden");
+  } 
+  else {
+    tryAgain.style.display = ("block");
+    result = tryAgain;
+    pythonMan.setAttribute("class", "hidden");
+    rubyLas.setAttribute("class", "hidden");
+    swiftWoman.setAttribute("class", "hidden");
   }
 
   
@@ -21,10 +50,7 @@ function surveySubmission(e) {
 
 
 
-  // const defencesInput = document.querySelector('input[name="defences"]:checked');
-  // const roleInput = document.querySelector('input[name="role"]:checked');
-  // const trainingInput = document.querySelector('input[name="training"]:checked');
-  // const petInput = document.querySelector('input[name="pet"]:checked');
+
 
 
 
